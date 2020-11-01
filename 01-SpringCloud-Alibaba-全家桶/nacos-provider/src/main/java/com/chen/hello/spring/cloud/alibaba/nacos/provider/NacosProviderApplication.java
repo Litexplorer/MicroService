e1cb7605ceac8ff714b8e95826a6a017dda8e65f
Litexplorer <1185964443@qq.com>
2020-11-01 22:08:51 +0800

    :ok: 添加了『统一依赖项目』以及『Nacos-Provider 项目』，没有测试

---
+++ 01-SpringCloud-Alibaba-全家桶/nacos-provider/src/main/java/com/chen/hello/spring/cloud/alibaba/nacos/provider/NacosProviderApplication.java
@@ -0,0 +1,34 @@
+package com.chen.hello.spring.cloud.alibaba.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ChromeChen
+ * @Description: Nacos 服务提供者
+ * @Date: Created in 22:03 2020/11/1 0001
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    /**
+     * 为了简洁，原来还可以直接作为内部类的写法 ^_^
+     */
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message;
+        }
+    }
+}
